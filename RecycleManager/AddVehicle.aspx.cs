b8269a57fc85fab306b25ac8cad91c1d7ce58740
Vishnu Vardhan Ramvath <s566760@nwmissouri.edu>
2024-09-18 09:38:22 -0500

    Added code implementation for Manage vehicle

---
+++ RecycleManager/AddVehicle.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RecycleManager
+{
+    public partial class AddVehicle : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGetVehicleId_Click(object sender, EventArgs e)
+        {
+            lblVehicleId.Text = Guid.NewGuid().ToString();
+        }
+
+        protected void Unnamed_SelectionChanged(object sender, EventArgs e)
+        {
+           txtPurchaseDate.Text =  cldPurchaseDate.SelectedDate.ToString();
+        }
+    }
+}
