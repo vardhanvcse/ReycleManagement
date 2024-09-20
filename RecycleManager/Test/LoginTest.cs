14c987e6af1bd3388f47d7e4b343b11271d98242
Ponamala Dwaraka Balaji <S566865@nwmissouri.edu>
2024-09-20 09:51:55 -0500

    Unit Test Setup

---
+++ RecycleManager/Test/LoginTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+namespace RecycleManager.Test
+{
+    [TestCase]
+    public class LoginTest
+    {
+        [Test]
+        public void LoginTestCheck() { 
+        }
+    }
+}
