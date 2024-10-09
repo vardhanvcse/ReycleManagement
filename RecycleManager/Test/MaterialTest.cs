1a54ab26db48863ab71f8cec747da41def69318e
Ponamala Dwaraka Balaji <S566865@nwmissouri.edu>
2024-10-09 09:50:01 -0500

    Added place holder for materialTest

---
+++ RecycleManager/Test/MaterialTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using RecycleManager.BusinessAccess;
+using RecycleManager.DataAccess;
+using RecycleManager.helpers;
+using Rhino.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecycleManager.Test
+{
+    public class MaterialTest
+    {
+        [TestCase]
+        public void GetMaterialTest()
+        {
+            MaterailBAL user = new MaterailBAL();    
+        }
+    }
+}
